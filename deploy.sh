fea41e5e3ec1ffd65307513d77d07b66093210f1
wanfxx <wanfxx@wanfxxdeMacBook-Pro.local>
2026-01-17 20:34:27 +0800

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git pull && rm -rf public && hexo deploy && echo "发布完成..."
+git pull && rm -rf public && hexo deploy && rm -rf ../atjaa.github.io/* && mv public/* ../atjaa.github.io/ && echo "b.atjaa.com" > ../atjaa.github.io/CNAME && echo "发布完成..."
